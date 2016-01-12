b72cc8555b10ce62eceb576b811b2ebc3a56d7c6
phitsc <phitsc@gmail.com>
2016-01-12 22:57:28 +0100

    Remove leet algorithms

--- src/txtproc/WebAlgorithms.d
+++ src/txtproc/WebAlgorithms.d
@@ -11,91 +11,11 @@ import std.stdio : writeln;
 import txtproc.algorithms;
 import txtproc.textalgo;
 
-private enum string[string] leetAlphabet =
-[
-    "a": "4",
-    "b": "8",
-    "c": "(",
-    "d": "|)",
-    "e": "3",
-    "f": "|=",
-    "g": "6",
-    "h": "|-|",
-    "i": "!",
-    "j": "_|",
-    "k": "|<",
-    "l": "1",
-    "m": "|\\/|",
-    "n": "|\\|",
-    "o": "0",
-    "p": "|°",
-    "q": "0_",
-    "r": "2",
-    "s": "5",
-    "t": "7",
-    "u": "|_|",
-    "v": "\\/",
-    "w": "\\/\\/",
-    "x": "%",
-    "y": "`/,",
-    "z": "2"
-];
 
 class WebAlgorithms : Algorithms
 {
     this()
     {
-        add(new Algorithm(
-            "ToLeet", "Web", "Convert input text to leet speak.", [],
-            (string text, string[], bool) {
-                string result;
-
-                foreach (character; text.chars)
-                {
-                    auto c = character.text in leetAlphabet;
-
-                    result ~= c ? *c : character.text;
-                }
-
-                return result;
-            }
-        ));
-
-        add(new Algorithm(
-            "FromLeet", "Web", "Convert input text from leet speak.", [],
-            (string text, string[], bool) {
-                string result;
-
-                while (text.length > 0)
-                {
-                    auto foundToken = false;
-
-                    foreach (key, value; leetAlphabet)
-                    {
-                        if (text.startsWith(value))
-                        {
-                            result ~= key;
-
-                            text = text[value.length .. $];
-
-                            foundToken = true;
-
-                            break;
-                        }
-                    }
-
-                    if (!foundToken)
-                    {
-                        result ~= text[0 .. 1];
-
-                        text = text[1 .. $];
-                    }
-                }
-
-                return result;
-            }
-        ));
-
         add(new Algorithm(
             "Tweet", "Web", "Break up input text into tweets.", [
                 ParameterDescription("Continuation text for each tweet", Default("")),
