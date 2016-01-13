5270c1b85c6bc25f7c457bcdc257b11776376ca5
phitsc <phitsc@gmail.com>
2016-01-13 20:50:35 +0100

    Add trivial writeline test

--- src/io.d
+++ src/io.d
@@ -5,3 +5,10 @@ void writeline(string text)
     import std.stdio;
     stdout.rawWrite(text ~ "\n");
 }
+
+unittest
+{
+    import std.exception;
+
+    assertNotThrown(writeline("writeline seems to work"));
+}
